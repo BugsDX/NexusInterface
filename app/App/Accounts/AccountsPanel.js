fbbef430d3cf2387ca2ed3dc99e15e832e3e8304
Kendal Cormany <kendal.cormany@nexus.io>
2019-06-07 17:16:55 -0700

    Accounts Page Wireframed out.

--- app/App/Accounts/AccountsPanel.js
+++ app/App/Accounts/AccountsPanel.js
@@ -4,11 +4,62 @@ import styled from '@emotion/styled';
 
 // Internal
 import TextField from 'components/TextField';
+import * as color from 'utils/color';
+import { consts, timing } from 'styles';
+import NexusAddress from 'components/NexusAddress';
 
-const PanelHolder = styled.div({
-  background: 'green',
+const PanelHolder = styled.div(({ theme }) => ({
+  background: color.lighten(theme.background, 0.2),
   flex: '1',
-  padding: '1rem',
+  padding: '0rem 0rem 1rem 0rem',
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  borderLeft: `thick solid ${theme.primary}`,
+}));
+
+const Title = styled.div(({ theme }) => ({
+  background: color.lighten(theme.background, 0.4),
+  color: theme.primary,
+  fontSize: '1.5em',
+  paddingLeft: '0.25em',
+}));
+
+const AccountsHolder = styled.div({
+  padding: '0.5rem 1rem 0rem 1rem',
+  flexGrow: 1,
+  flexBasis: '35em',
+  overflow: 'auto',
+});
+
+const AccountBox = styled.div(({ theme }) => ({
+  background: color.lighten(theme.background, 0.4),
+  border: `1px solid ${theme.primary}`,
+  borderRadius: '2.5px',
+  margin: '1em',
+  display: 'grid',
+  gridTemplateColumns: 'auto auto',
+  gridTemplateRows: 'auto',
+  transition: `background ${timing.normal}`,
+  '&:hover': {
+    background: color.darken(theme.background, 0.2),
+  },
+}));
+
+const AccountBoxRight = styled.div({
+  float: 'right',
+  margin: 'auto 0% auto auto ',
+  paddingRight: '.5em',
+  display: 'inlineBlock',
+});
+
+const AccountBoxLeft = styled.div({
+  paddingLeft: '.5em',
+  paddingBottom: '.5em',
+});
+
+const NXSAddress = styled(NexusAddress)({
+  fontSize: '.75em',
 });
 
 class AccountsPanel extends Component {
@@ -27,37 +78,47 @@ class AccountsPanel extends Component {
     const accountstemp = [
       {
         name: 'Default',
-        address: 'asdsasdsadsdaasdasddsaas',
+        address:
+          'a74774dc075e59d03639b56da8c29736fe248888a43c6103060409dd11273417',
         token: 'NXS',
-        tokenaddress: 'ppppppppppppppppppppppp',
+        tokenaddress:
+          'bf501d4f3d81c31f62038984e923ad01546ff678e305a7cc11b1931742524ce1',
         balance: '99999',
       },
       {
         name: 'Savings',
-        address: 'fffffffffffffffffff',
+        address:
+          'a74774dc075e59d03639b56da8c29736fe248888a43c6103060409dd11273417',
         token: 'mytoken',
-        tokenaddress: 'dddddddddddddddddd',
+        tokenaddress:
+          'bf501d4f3d81c31f62038984e923ad01546ff678e305a7cc11b1931742524ce1',
         balance: '8080',
       },
       {
-        name: 'Savings',
-        address: 'fffffffffffffffffff',
+        name: 'Savings2',
+        address:
+          'a74774dc075e59d03639b56da8c29736fe248888a43c6103060409dd11273417',
         token: 'mytoken',
-        tokenaddress: 'dddddddddddddddddd',
+        tokenaddress:
+          'bf501d4f3d81c31f62038984e923ad01546ff678e305a7cc11b1931742524ce1',
         balance: '8080',
       },
       {
-        name: 'Savings',
-        address: 'fffffffffffffffffff',
+        name: 'Saving3s',
+        address:
+          'a74774dc075e59d03639b56da8c29736fe248888a43c6103060409dd11273417',
         token: 'mytoken',
-        tokenaddress: 'dddddddddddddddddd',
+        tokenaddress:
+          'bf501d4f3d81c31f62038984e923ad01546ff678e305a7cc11b1931742524ce1',
         balance: '8080',
       },
       {
-        name: 'Savings',
-        address: 'fffffffffffffffffff',
+        name: 'Savings4',
+        address:
+          'a74774dc075e59d03639b56da8c29736fe248888a43c6103060409dd11273417',
         token: 'mytoken',
-        tokenaddress: 'dddddddddddddddddd',
+        tokenaddress:
+          'bf501d4f3d81c31f62038984e923ad01546ff678e305a7cc11b1931742524ce1',
         balance: '8080',
       },
     ];
@@ -75,25 +136,24 @@ class AccountsPanel extends Component {
 
   makeBox(e) {
     return (
-      <div style={{ background: 'black', padding: '1em' }}>
-        {e.name}
-        <br />
-        {e.address}
-        <br />
-        {e.token}
-        <br />
-        {e.tokenaddress}
-        <br />
-        {e.balance}
-      </div>
+      <AccountBox>
+        {' '}
+        <AccountBoxLeft>
+          <div>{e.name}</div>
+          <NXSAddress key={e.address} address={e.address} />
+          <div>{e.token}</div>
+          <NXSAddress key={e.tokenaddress} address={e.tokenaddress} />
+        </AccountBoxLeft>
+        <AccountBoxRight>{`Balance: ${e.balance}`}</AccountBoxRight>
+      </AccountBox>
     );
   }
 
   render() {
     return (
-      <PanelHolder>
-        {'Accounts'}
-        <div style={{ overflow: 'scroll' }}>{this.returnBoxes()}</div>
+      <PanelHolder style={{ overflow: 'none' }}>
+        <Title>{'Accounts'}</Title>
+        <AccountsHolder>{this.returnBoxes()}</AccountsHolder>
       </PanelHolder>
     );
   }
