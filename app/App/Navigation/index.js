cdb82e8931cecf75505b763f2ae7297953117c9c
Dillon <dugan_dillon@yahoo.com>
2019-06-14 17:41:01 -0700

    woops

--- app/App/Navigation/index.js
+++ app/App/Navigation/index.js
@@ -24,8 +24,11 @@ import transactionsIcon from 'images/transaction.sprite.svg';
 import addressBookIcon from 'images/address-book.sprite.svg';
 import settingsIcon from 'images/settings.sprite.svg';
 import consoleIcon from 'images/console.sprite.svg';
+import userIcon from 'images/user.sprite.svg';
+import tokensIcon from 'images/nxs-staking.sprite.svg';
 // import shapeshiftIcon from 'images/shapeshift.sprite.svg';
 // import trustListIcon from 'images/trust-list.sprite.svg';
+//
 
 const slideUp = keyframes`
   from { opacity: 0; transform: translateY(70%) }
